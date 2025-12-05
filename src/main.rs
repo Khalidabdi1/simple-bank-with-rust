0c14a24caeab2c1012d0cdfe605b074bbca6f327
Khalid Abdi <xamza2009@gmail.com>
2025-12-05 23:26:43 +0300

    finish show

--- src/main.rs
+++ src/main.rs
@@ -23,8 +23,7 @@ fn main() {
         println!("3. Withdraw");
         println!("4. Transfer");
         println!("5. Show account");
-        println!("6. Deposit");
-        println!("7. Exit");
+        println!("6. Exit");
         print!("Enter number to start : ");
         io::stdout().flush().unwrap();
 
@@ -55,11 +54,10 @@ fn main() {
         }else if choose==4 {
             
         }else if choose==5 {
+            show_account(&mut info);
             
         }else if choose==6 {
-           
-        }else if choose==7 {
-            break;
+           break;
         }
         
     }
@@ -72,12 +70,15 @@ struct Account{
     id:i32,
     name:String,
     email:String,
-    blance:f64
+    blance:f64,
+    password:String
 
 }
 
+
+// for create
 fn create_account(info:&mut HashMap<String,Account>){
-     let mut id:i32=0;
+     let _id:i32=0;
 
 
    println!("-------------------");
@@ -96,8 +97,28 @@ fn create_account(info:&mut HashMap<String,Account>){
     io::stdout().flush().unwrap();
 
     let mut email:String =String::new();
+
+loop{
     io::stdin().read_line(&mut email).unwrap();
     let email=email.trim().to_string();
+
+   if email.contains('@'){
+
+break;
+   }else {
+      println!("Write your email :");
+       println!("your email don't have @")
+   }
+}
+
+
+
+    print!("Write your password :");
+    io::stdout().flush().unwrap();
+
+    let mut password :String=" ".to_string();
+    io::stdin().read_line(&mut password).expect("not a pssword");
+    let password:String=password.trim().to_string();
 
 
     let id =info.len() as i32+1;
@@ -106,18 +127,21 @@ fn create_account(info:&mut HashMap<String,Account>){
         id :id,
          name:name.to_string(),
           email:email,
+          password:password,
            blance:0.0
     };
 
  
  
     info.insert(user.id.to_string(), user);
+    println!("account is :{:?} ",info);
 
       match info.get(&id.to_string()){
     Some(d)=>{
         println!("the name is : {}",d.name);
         println!("the email is : {}",d.email);
         println!("the blance is {}",d.blance);
+        println!("the password is : {}" ,d.password);
          println!("The account has been created successfully 🏦");
     }
     None =>println!("not found")
@@ -128,5 +152,57 @@ fn create_account(info:&mut HashMap<String,Account>){
 
      println!("-------------------");
    
+
+}
+
+
+
+// show account
+
+fn show_account(info:&mut HashMap<String,Account>){
+
+    println!("To view your account information, please enter your account number and password :");
+
+    print!(" please enter your account number : ");
+    io::stdout().flush().unwrap();
+
+    let mut account:String=String::new();
+    io::stdin().read_line(&mut account).unwrap();
+
+    let account =account.trim().to_string();
+
+    match info.get(&account){
+        Some(e) =>{
+            println!("account found");
+            println!("the account is : {:?}", e);
+            check(&e.password,e);
+
+        }
+        None =>println!("not found this account")
+    }
+
+    // ask for password
+
+    fn check(pass:&String,info:&Account){
+        
+        let mut password:String=String::new();
+        print!("write the password :");
+        io::stdout().flush().unwrap();
+
+        io::stdin().read_line(&mut password).unwrap();
+
+        let password:String=password.trim().to_string();
+
+        if password==*pass {
+            println!("Your account information is :");
+            println!("your name is {} and your blance is :{}", info.name,info.blance);
+
+        }else {
+            println!("the password not correct !")
+        }
+
+    }
+
+
 
 }
